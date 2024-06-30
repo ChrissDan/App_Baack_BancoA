8cffd24512295d2e7712d021e27fb02deb8103e5
ChrissDan <Laboulade2@gmail.com>
2024-06-30 12:20:51 -0500

    v0.0.2

--- bancoA/src/main/java/api_rest_backend/bancoA/configuration/WebConfig.java
+++ bancoA/src/main/java/api_rest_backend/bancoA/configuration/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://app-front-banco-a.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
